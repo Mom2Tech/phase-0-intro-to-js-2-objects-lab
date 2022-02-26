c1183d9ecdb391cbca4ee2371c961743ac1af522
Kim Green <kimgreen.writings@gmail.com>
2022-02-26 16:17:14 +0200

    commit tree 06af5da76d9a95124df74e5084a4c49b167f7b4d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+// To start, define a employee variable
+//  and assign it to an Object containing name and streetAddress keys;
+
+let employees = {
+    name: "Test", 
+    streetAdress: "123 Main St"
+};
+// updateEmployeeWithKeyAndValue(): this function should take in three
+//  arguments: an employee Object, 
+//  a key and a value. This function should not mutate the employee; 
+//  it should return a new Object 
+//  that has an updated value for the key passed in. 
+//  Hint: use the spread operator!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+//Dynamic
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee = {...employee} 
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key];
+    return employee;
+}
